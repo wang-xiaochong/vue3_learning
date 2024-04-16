96b11167485cacf19251541a39c2e866759eaaa0
wangxiaochong <1378334435@qq.com>
2024-04-16 16:26:45 +0800

    multiple app demo

--- 备份/count_demo/main.ts
+++ 备份/count_demo/main.ts
@@ -1,19 +1,10 @@
 import { createApp } from "vue";
 import App from './App.vue'
-import App2 from './App2.vue'
 
 const app = createApp(App)
 app.mount('#app')
 
 app.config.errorHandler = (err) => {
-    /* 处理错误 */
-    console.log('err', err);
-}
-
-const app2 = createApp(App2)
-app2.mount('#app2')
-
-app2.config.errorHandler = (err) => {
     /* 处理错误 */
     console.log('err', err);
 }
